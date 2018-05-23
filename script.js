54452a9a72e1d4d028c59335cf0be26337531ed2
theAprel <cpindziak@alumni.ufl.edu>
2018-05-23 14:37:39 -0400

    Add button controls for chat

--- script.js
+++ script.js
@@ -15,9 +15,38 @@ const popout_chat = 'https://www.twitch.tv/popout/' + chat_host + '/chat';
 var chat_element = $("#chatwrap");
 chat_element.html(chat_frame);
 
-$('<div/>', {
-    html: '<a href="' + popout_chat + '" target="_blank">Popout chat</a>'
-}).appendTo("#chatwrap");
+var hide_chat_text = "Hide chat";
+var is_chat_hidden = false;
+const button_css_class = "btn btn-sm btn-default";
+var hide_chat_button = $('<button/>', {
+    "class": button_css_class,
+    text: hide_chat_text,
+    click: function() {
+        if(is_chat_hidden) {
+            chat_element.show();
+            hide_chat_button.text(hide_chat_text);
+            is_chat_hidden = false;
+        }
+        else {
+            chat_element.hide();
+            hide_chat_button.text("Show chat");
+            is_chat_hidden = true;
+        }
+    }
+});
+
+var button_div = $("#leftcontrols");
+button_div.html("");
+button_div.append(hide_chat_button);
+
+var popout_chat_button = $('<button/>', {
+    "class": button_css_class,
+    text: "Popout chat",
+    click: function() {
+        window.open(popout_chat, '_blank', 'location=yes,height=570,width=350,scrollbars=yes,status=yes');
+    }
+});
+button_div.append(popout_chat_button);
 
 var resize_chat = function() {
     $("#" + chat_host).attr("height", $("#videowrap").height());
